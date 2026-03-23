20657fa6b41a73bc097dedd3cc24dffced356306
Lippi-Ugo <ugo.lippi@edu.univ-fcomte.fr>
2026-03-23 21:43:34 +0100

    migration

---
+++ pocketbase/pb_migrations/1774270828_updated_aliment.js
@@ -0,0 +1,28 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3786205377")
+
+  // update collection data
+  unmarshal({
+    "createRule": "@request.auth.verified = true",
+    "deleteRule": "@request.auth.id != \"\"",
+    "listRule": "@request.auth.id != \"\"",
+    "updateRule": "@request.auth.verified = true",
+    "viewRule": "@request.auth.id != \"\""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3786205377")
+
+  // update collection data
+  unmarshal({
+    "createRule": null,
+    "deleteRule": null,
+    "listRule": null,
+    "updateRule": null,
+    "viewRule": null
+  }, collection)
+
+  return app.save(collection)
+})
